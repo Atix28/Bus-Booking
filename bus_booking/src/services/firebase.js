a75c710c0eaf0e6ad1bb7bfdc383e44f7ad963d7
Isuru Liyanage <isuruliyanage1@gmail.com>
2019-09-28 21:02:28 +0530

    firebase database updated

--- bus_booking/src/services/firebase.js
+++ bus_booking/src/services/firebase.js
@@ -3,18 +3,19 @@ import  'firebase/auth';
 import  'firebase/firestore';
 
 
- const firebaseConfig = {
-    apiKey: "AIzaSyAtfeYcDX3YE7JpwGGxziTxhvmCKJRevwo",
-    authDomain: "bus-book-project.firebaseapp.com",
-    databaseURL: "https://bus-book-project.firebaseio.com",
-    projectId: "bus-book-project",
-    storageBucket: "",
-    messagingSenderId: "387936206995",
-    appId: "1:387936206995:web:7fb92dc87adf47b12f8d66",
-    measurementId: "G-TNWBZGZXS9"
-  }
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
+var firebaseConfig = {
+  apiKey: "AIzaSyC_L57KOfg7-3J695v3Nv9T06ntaHhfhjM",
+  authDomain: "bus-ticket-app-7c550.firebaseapp.com",
+  databaseURL: "https://bus-ticket-app-7c550.firebaseio.com",
+  projectId: "bus-ticket-app-7c550",
+  storageBucket: "bus-ticket-app-7c550.appspot.com",
+  messagingSenderId: "850686102896",
+  appId: "1:850686102896:web:00f5a419c96a261a2a13b5",
+  measurementId: "G-8PYTB2XTLY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 
 export const auth = firebase.auth()
